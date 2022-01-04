efa1af02bbdaf989ea532f40ce3d83857dec1e38
aritra-tech <aritrarick2002@gmail.com>
2022-01-04 16:55:58 +0530

    Splash , Home and Authentication Screen Done ✔

---
+++ app/src/main/java/com/geekym/presinadminpanel/Authentication/RegisterScreen.java
@@ -0,0 +1,16 @@
+package com.geekym.presinadminpanel.Authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.geekym.presinadminpanel.R;
+
+public class RegisterScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_screen);
+    }
+}
